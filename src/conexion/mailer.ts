77290ea7630bcf6f9468fb0c03ee3e6a369f6fac
Luis-Flores <lf016158@gmail.com>
2023-07-24 08:28:47 -0600

    se termino la api para recuperacion de contraseña

--- src/conexion/mailer.ts
+++ src/conexion/mailer.ts
@@ -1,15 +1,26 @@
 const nodemailer = require("nodemailer");
 
+
+
 exports.sendEmail = async function (_req, res,data) {
     // Definimos el transporter
     var transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+        host: process.env.correo_servicio,
+        port: process.env.correo_port,
         auth: {
-          user: "319e92345f67a7",
-          pass: "0a110133da1ab6"
+          user: process.env.user,
+          pass: process.env.correo_contrasenia
         }
       });
+ const datajson = [{
+    host: process.env.correo_servicio,
+    port: process.env.correo_port,
+    auth: {
+      user: process.env.user,
+      pass: process.env.correo_contrasenia
+    }
+  }];
+      console.log(datajson);
 
     
     // Definimos el email
@@ -22,9 +33,12 @@ exports.sendEmail = async function (_req, res,data) {
     // Enviamos el email
     await transport.sendMail(mailOptions, function (error, _info) {
         if (error) {
+            console.log("eRROR DE ENVIO: " + error);
             return res.json({error: "Error de envio: " + error.message });
+          
             //msj("Error de Envió",error.message,200,[],res);
         } else {
+            console.log("ENVIO EXITOSO");
             return res.json({data: data.mensajeConfirmacion});
            // msj("Envió Exitoso",data.mensajeConfirmacion,200,[],res);
         }
